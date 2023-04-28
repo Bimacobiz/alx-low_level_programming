cb462da3211817b072c8fb0f359864f6fdae2523
Bimacobiz <collinsmahigi@gmail.com>
2023-04-27 20:30:00 -0700

    singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,7 +5,7 @@
  * add_node - adds a new node at the beginning of a list_t list.
  * @head: it is a double pointer to the list_t list
  * @str: is a new string that should be added to the node
- * Return: head
+ * Return: address to the new element or Null if it fails
  */
 list_t *add_node(list_t **head, const char *str)
 {
