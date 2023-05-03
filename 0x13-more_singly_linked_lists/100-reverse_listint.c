7da072202a670e462b8f022a6b64415a2b7e2ff5
Bimacobiz <collinsmahigi@gmail.com>
2023-05-03 06:00:47 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- *reverse_listint - reverses a listint_t linked list
- *@head: a double pointer to the head of a linked list
- *Returns: a pointer to the first node of the reversed list
+ * reverse_listint - reverses a listint_t linked list
+ * @head: a double pointer to the head of a linked list
+ * Return: a pointer to the first node of the reversed list
  */
 listint_t *reverse_listint(listint_t **head)
 {
