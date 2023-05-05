ad837d7ff4709c86a812670532244c436783e4ba
Bimacobiz <collinsmahigi@gmail.com>
2023-05-05 09:17:00 -0700

    bit_manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,7 +2,7 @@
 /**
  *get_bit - prints the binary representation of a number
  *@index: the index, starting from 0 of the bit you want to get
- *@n: a parameter 
+ *@n: a parameter
  *Return: the value of the bit at index index or -1 if an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
