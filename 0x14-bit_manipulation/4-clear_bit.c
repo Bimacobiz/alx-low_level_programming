fd794214a86212d0e66fd21f394007a3641d70d5
Bimacobiz <collinsmahigi@gmail.com>
2023-07-06 18:05:57 -0700

    ammendment to task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *clear_bit - a function that sets a bit's value to 0 at a particular index
- *@n: points to the number that should be changed 
+ *@n: points to the number that should be changed
  *@index: the index of the bit that should be cleared
  *Return: 1 if it succeeds, -1 if it fails
  */
