9413ce6b0e4b4ecb2cd9bf6a8d18584dda487c43
Bimacobiz <collinsmahigi@gmail.com>
2023-07-06 18:17:25 -0700

    task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *flip_bits - returns the bits that should be flipped to move from on no. to the next
+ *@n: the initial number
+ *@m: the following number
+ *Return: the number of bits that should be changed
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int c, tally = 0;
+	unsigned long int present;
+	unsigned long int entire = n ^ m;
+
+	for (c = 63; c >= 0; c--)
+	{
+		present = entire >> c;
+		if (present & 1)
+			tally++;
+	}
+
+	return (tally);
+}
