c00c5f4494ab2c550f9c06336c12a3c6fea2e311
Bimacobiz <collinsmahigi@gmail.com>
2023-07-10 15:55:41 -0700

    ammendments to task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,27 +8,30 @@
  *read_textfile - reads a text file then prints it to the posix std output
  *@filename: a pointer to the file that the function should read
  *@letters: the maximum number of letter that the function can read
+ *Return: letters read
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fdvalue;/*fdvalue is the file descriptor value*/
 	ssize_t letters_read;
 
-	char *buffer = malloc((1024)*sizeof(char));
+	char *buffer = malloc((1024) * sizeof(char));
+
 	fdvalue = open(filename, O_RDONLY);
 
 	if (fdvalue == -1)
-		return -1;
+		return (-1);
 
 	letters_read = read(fdvalue, buffer, letters);
+
 	if (letters_read == -1)
 	{
 		close(fdvalue);
-		return -1;
+		return (-1);
 	}
 
 	write(STDOUT_FILENO, buffer, letters_read);
 	close(fdvalue);
 
-	return letters_read;
+	return (letters_read);
 }
