a7cdab6d2cc3cec22485c4c8260a59a9590d5d52
Bimacobiz <collinsmahigi@gmail.com>
2023-08-17 17:23:31 -0700

    ammendments

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,17 +1,18 @@
 #include "lists.h"
 /**
  *dlistint_len - returns the number of elements that are contained
- *in a linked dlistint_t list. 
+ *in a linked dlistint_t list.
  *@h: points to the head of the list
  *Return: Number of elements in the given list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
 	int ele_no = 0;
+
 	while (h != NULL)
-		{
-			ele_no++;
-			h = h->next;
-		}
+	{
+		ele_no++;
+		h = h->next;
+	}
 	return(ele_no);
 }
