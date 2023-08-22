e283a21ddd7884fbd650c859efdcd60aa0321d57
Bimacobiz <collinsmahigi@gmail.com>
2023-08-21 19:47:22 -0700

    ammendments

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -O /tmp/win.so https://github.com/Bimacobiz/alx-low_level_programming.git/raw/master/0x18-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+export LD_PRELOAD=/tmp/win.so1
