dc9bed8587f9057832481a62902dbbd06f961925
Bimacobiz <collinsmahigi@gmail.com>
2023-08-21 19:42:57 -0700

    adavanced task

---
+++ 0x18-dynamic_libraries/calculations.c
@@ -0,0 +1,22 @@
+int add(int a, int b)
+{
+	return (a + b);
+}
+int sub(int c, int d)
+{
+	return (c - d);
+}
+
+int mod(int c, int d)
+{
+	return (c % d);
+}
+
+int mul(int c, int d)
+{
+	return (c * d);
+}
+int div(int c, int d)
+{
+	return (c / d);
+}
