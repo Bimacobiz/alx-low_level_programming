ce131234979ac885525f6e6290c59e44f2bfbb5d
Bimacobiz <collinsmahigi@gmail.com>
2023-09-08 19:51:59 -0700

    task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints the hash table
+ * @ht: points to hash table
+ *
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int a = 0;
+	hash_node_t  *store;
+	int not_fin = 0;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	for (a = 0; a < ht->size; a++)
+	{
+		bucket = ht->array[a];
+		while (store)
+		{
+			if (not_fin)
+				printf(", ");
+			printf("'%s': '%s'", store->key, store->value);
+			not_fin = 1;
+			store = store->next;
+		}
+	}
+	printf("}\n");
+}
