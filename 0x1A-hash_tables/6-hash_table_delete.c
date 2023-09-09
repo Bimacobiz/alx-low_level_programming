a058fa1af2f88037818e78c6bfada75074d2bbc2
Bimacobiz <collinsmahigi@gmail.com>
2023-09-08 19:58:50 -0700

    tasl 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: points to the hash table
+ *
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *store, *aux_del;
+	unsigned long int a = 0;
+
+	if (!ht)
+		return;
+
+	for (a = 0; a < ht->size; a++)
+	{
+		bucket = ht->array[a];
+		while (store)
+		{
+			aux_del = store;
+			store = store->next;
+			if (aux_del->key)
+				free(aux_del->key);
+			if (aux_del->value)
+				free(aux_del->value);
+			free(aux_del);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
